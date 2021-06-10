84bcf9029490d3b5395826ae5f0a7db0323b39c6
fusion-steel <lethal@mail.ua>
2021-06-10 15:15:11 +0300

    Feat: implement toTextDay method.

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,22 @@
 package subtask1
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 class DateFormatter {
 
-    // TODO: Complete the following function
+    /**
+     * This method converts date representation given in Strings (day, month, year)
+     * to format dd MMMM, EEEE (e.g. 15 3 2020 to 15 марта, воскресенье)
+     */
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+        val date = GregorianCalendar(year.toInt(), month.toInt() - 1, day.toInt())
+        date.isLenient = false
+        val formatter = SimpleDateFormat("dd MMMM, EEEE", Locale("ru"))
+        return try {
+            formatter.format(date.time).also { println(it) }
+        } catch (e: IllegalArgumentException) {
+            "Такого дня не существует"
+        }
     }
 }
